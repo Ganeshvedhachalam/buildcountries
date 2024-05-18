6f4f10f2e7665e52b8fc2003ac3044673f8e5021
GANESH <96299576+Ganeshvedhachalam@users.noreply.github.com>
2024-05-18 13:42:14 +0000

    countries search implementation done

--- countryflag/src/country.jsx
+++ countryflag/src/country.jsx
@@ -1,66 +1,108 @@
 import React, { useEffect, useState } from "react";
-const  Country = () => {
-const [countries,setcountries]= useState([]);
-const [error, setError] = useState(null);
+const Country = () => {
+  const [countries, setcountries] = useState([]);
+  const [error, setError] = useState(null);
+  const [searchdata, setSearchdata] = useState("");
 
-    const apiUrl = "https://restcountries.com/v3.1/all"
+  const apiUrl = "https://restcountries.com/v3.1/all";
 
-    const Tile =({country}) =>{
-        return(<>
-       
-          
-         <div className="country" 
-         style={{display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"center",width:"200px",
-         border:"1px solid gray",borderRadius:"10px",margin:"10px",padding:"10px"
-         }}>
-            
-            
-            <img  style={{width:"100px",height:"100px"}} src={country.flags.png} alt="flag" />
-            <h1>{country.name.common} </h1>
-            
-            
-         </div>
-
-       
-
-
-
-
-        
-        </>
-        )
-    }
-
-    useEffect(() => { 
-        fetch(apiUrl)
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error("Failed to fetch data ");
-                }
-                return response.json();
-            })
-            .then((data) => setcountries(data))
-            .catch((error) => {
-                console.error("Error fetching data : ", error);
-                setError(error.message);
-            });
-    }, []);
-
-
-    return(
-        <>
-        {error?<div>The Error: {error}</div>:
-         <div className="COUNTRYCARDS" 
-         style={{ display:"flex",flexWrap:"wrap",justifyContent:"center",alignItems:"center" ,height:"100vh"}}
-         >
-           
-             {countries.map((country )=> <Tile key={country.cca3} country={country}/> )}
- 
-             </div>
- 
-        }
-        
-        </>
+  const Tile = ({ country }) => {
+    return (
+      <>
+        <div
+          className="countryCard"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "200px",
+            height: "200px",
+            border: "1px solid gray",
+            borderRadius: "10px",
+            margin: "10px",
+            padding: "10px",
+          }}
+        >
+          <img
+            style={{ width: "100px", height: "100px" }}
+            src={country.flags.png}
+            alt="flag"
+          />
+          <h1>{country.name.common} </h1>
+        </div>
+      </>
     );
-}
+  };
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch data ");
+        }
+        return response.json();
+      })
+      .then((data) => setcountries(data))
+      .catch((error) => {
+        console.error("Error fetching data : ", error);
+        setError(error.message);
+      });
+  }, []);
+
+  const HandleSearch = (event) => {
+    setSearchdata(event.target.value);
+  };
+
+  let filteredcountries = countries;
+  if (searchdata) {
+    filteredcountries = countries.filter((country) =>
+      country.name.common.toLowerCase().includes(searchdata.toLowerCase()),
+    );
+  }
+
+  return (
+    <>
+      {error ? (
+        <div>The Error: {error}</div>
+      ) : (
+        <div
+          className="COUNTRYCARDS"
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            // alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          <div
+            className="navbar"
+            style={{
+              width: "100%",
+              height: "50px",
+              backgroundColor: "gray",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <input
+              className="searchbar"
+              value={searchdata}
+              onChange={HandleSearch}
+              size={70}
+              type="text"
+              placeholder="search for a country "
+            />
+          </div>
+
+          {filteredcountries.map((country) => (
+            <Tile key={country.cca3} country={country} />
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
 export default Country;
